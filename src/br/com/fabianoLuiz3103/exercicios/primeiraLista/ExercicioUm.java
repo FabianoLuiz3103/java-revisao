249c8d34dad3047db50de0ba166ba931b822128e
FabianoLuiz3103 <fabianojesus1991@gmail.com>
2025-01-09 12:08:01 -0300

    Exercício 01 da primeira lista

---
+++ src/br/com/fabianoLuiz3103/exercicios/primeiraLista/ExercicioUm.java
@@ -0,0 +1,40 @@
+package br.com.fabianoLuiz3103.exercicios.primeiraLista;
+
+import java.util.Scanner;
+
+/**
+ * @author FabianoLuiz3103
+ * --> Faça um programa que peça dois números e imprima a soma.
+ */
+
+public class ExercicioUm {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        int numeroUm = 0;
+        int numeroDois = 0;
+        boolean entradaValida = false;
+
+        while(!entradaValida){
+            try{
+                System.out.println("Digite o primeiro número: ");
+                numeroUm = scanner.nextInt();
+                scanner.nextLine();
+
+                System.out.println("Digite o segundo número: ");
+                numeroDois = scanner.nextInt();
+
+                entradaValida = true;
+
+            }catch (Exception e) {
+                System.out.println("O valor informado deve ser um número inteiro! \n");
+                scanner.nextLine();
+            }
+        }
+
+        System.out.println("A soma de " + numeroUm + " com " + numeroDois + " é igual a: " + (numeroUm+numeroDois));
+        scanner.close();
+
+    }
+}
