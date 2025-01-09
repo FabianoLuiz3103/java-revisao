9fcbe550b9123a045d303a3a6d7b71f8e6b174aa
FabianoLuiz3103 <fabianojesus1991@gmail.com>
2025-01-09 13:31:41 -0300

    Exercício 05 da primeira lista

---
+++ src/br/com/fabianoLuiz3103/exercicios/primeiraLista/Exercicio05.java
@@ -0,0 +1,38 @@
+package br.com.fabianoLuiz3103.exercicios.primeiraLista;
+
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
+/**
+ * @author FabianoLuiz3103
+ * --> Faça um programa que peça uma temperatura em Farenheit e converta para Celsius.
+ */
+public class Exercicio05 {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        DecimalFormat df = new DecimalFormat("0.00");
+        double temperaturaFarenheit = 0.0;
+        double temperaturaCelsius = 0.0;
+
+
+        while(true){
+            try{
+                System.out.println("\nInforme uma temperatura em graus Farenheit (°F): ");
+                temperaturaFarenheit = scanner.nextDouble();
+                break;
+            }catch (Exception e){
+                System.out.println("A temperatura deve ser um valor numérico!! ");
+                scanner.nextLine();
+            }
+        }
+
+        temperaturaCelsius =  (5*(temperaturaFarenheit-32)/9);
+        String temperaturaCelsiusFormat = df.format(temperaturaCelsius);
+
+        System.out.println("\n" + temperaturaFarenheit + "°F equivale a " + temperaturaCelsiusFormat + "°C");
+
+        scanner.close();
+    }
+}
