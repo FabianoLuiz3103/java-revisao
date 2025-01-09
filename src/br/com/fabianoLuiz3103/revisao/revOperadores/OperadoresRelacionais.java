c80dbeb6d6cf38e481ccdec6cf03c67c42e98365
FabianoLuiz3103 <fabianojesus1991@gmail.com>
2025-01-09 11:34:14 -0300

    Operadores relacionais

---
+++ src/br/com/fabianoLuiz3103/revisao/revOperadores/OperadoresRelacionais.java
@@ -0,0 +1,17 @@
+package br.com.fabianoLuiz3103.revisao.revOperadores;
+
+public class OperadoresRelacionais {
+
+    public static void main(String[] args) {
+
+        int valorUm = 1;
+        int valorDois = 2;
+
+        System.out.println("ValorUm == ValorDois: " + (valorUm == valorDois));
+        System.out.println("ValorUm != ValorDois: " + (valorUm != valorDois));
+        System.out.println("ValorUm > ValorDois: " + (valorUm > valorDois));
+        System.out.println("ValorUm >= ValorDois: " + (valorUm >= valorDois));
+        System.out.println("ValorUm < ValorDois: " + (valorUm < valorDois));
+        System.out.println("ValorUm <= ValorDois: " + (valorUm <= valorDois));
+    }
+}
