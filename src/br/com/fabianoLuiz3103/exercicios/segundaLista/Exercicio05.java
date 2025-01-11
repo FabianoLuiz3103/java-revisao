fd73d3c6a532b6e2fd93c182c6aab0a8c3d238cd
FabianoLuiz3103 <fabianojesus1991@gmail.com>
2025-01-11 13:09:42 -0300

    Exercício 05 da segunda lista

---
+++ src/br/com/fabianoLuiz3103/exercicios/segundaLista/Exercicio05.java
@@ -0,0 +1,51 @@
+package br.com.fabianoLuiz3103.exercicios.segundaLista;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+/**
+ * @author FabianoLuiz3103
+ * --> Faça um programa que receba 2 notas parciais de um aluno, calcule a média e retorne se está aprovado ou não
+ */
+public class Exercicio05 {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        double[] notas = new double[2];
+        double media;
+
+        for(int i = 0; i < notas.length; i++){
+
+            while (true){
+
+                try {
+                    System.out.println("\nInforme sua " + (i+1) + "° nota:");
+                    notas[i] = scanner.nextDouble();
+
+                    if(notas[i] < 0 || notas[i] > 10){
+                        System.out.println("\nA nota deve estar entre 0 e 10");
+                        continue;
+                    }
+                    break;
+
+                }catch (InputMismatchException e){
+                    System.out.println("\nO valor informado deve ser do tipo numérico! ");
+                    scanner.nextLine();
+                }
+            }
+        }
+
+        media = (notas[0]+notas[1])/2;
+
+        if(media == 10){
+            System.out.println("\n\tSUA MÉDIA É: " + media + " VOCÊ ESTÁ APROVADO COM DISTINÇÃO! ");
+        } else if(media >= 7) {
+            System.out.println("\n\tSUA MÉDIA É: " + media + " VOCÊ ESTÁ APROVADO! ");
+        } else {
+            System.out.println("\n\tSUA MÉDIA É: " + media + " VOCÊ ESTÁ REPROVADO! ");
+        }
+
+        scanner.close();
+    }
+}
