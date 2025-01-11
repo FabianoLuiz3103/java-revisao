f3c069fbff1518b060c85c1c26732d0a4a10c966
FabianoLuiz3103 <fabianojesus1991@gmail.com>
2025-01-11 12:10:08 -0300

    Exercício 03 da segunda lista

---
+++ src/br/com/fabianoLuiz3103/exercicios/segundaLista/Exercicio03.java
@@ -0,0 +1,37 @@
+package br.com.fabianoLuiz3103.exercicios.segundaLista;
+
+import java.util.Scanner;
+
+/**
+ * @author FabianoLuiz3103
+ * --> Faça um programa que receba duas letras: "F" ou "M";
+ * "F" exibir feminino
+ * "M" exibir masculino
+ * outra letra - inválido
+ */
+public class Exercicio03 {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        String sexo;
+
+        System.out.println("\nInforme o sexo (M/F): ");
+        sexo = scanner.next().toUpperCase();
+
+        switch (sexo){
+
+            case "F":
+                System.out.println("FEMININO!");
+            break;
+            case "M":
+                System.out.println("MASCULINO! ");
+            break;
+            default:
+                System.out.println("SEXO INVÁLIDO! ");
+            break;
+        }
+
+        scanner.close();
+    }
+}
