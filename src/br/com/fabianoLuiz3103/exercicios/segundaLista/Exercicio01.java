fa90378563349124d101dc80c2d6b5ea6b21d638
FabianoLuiz3103 <fabianojesus1991@gmail.com>
2025-01-11 11:46:40 -0300

    Exercício 01 da segunda lista

---
+++ src/br/com/fabianoLuiz3103/exercicios/segundaLista/Exercicio01.java
@@ -0,0 +1,37 @@
+package br.com.fabianoLuiz3103.exercicios.segundaLista;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+/**
+ * @author FabianoLuiz3103
+ * --> Faça um programa que peça dois números e imprima o maior deles
+ */
+public class Exercicio01 {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        double[] numeros = new double[2];
+
+        for(int i = 0; i < numeros.length; i++){
+            while (true){
+                try{
+                    System.out.println("Informe o " + (i+1) + "º número: ");
+                    numeros[i] = scanner.nextDouble();
+                    break;
+                }catch (InputMismatchException e){
+                    System.out.println("O número deve ser um valor do tipo numérico! ");
+                    scanner.nextLine();
+                }
+            }
+        }
+        if(numeros[0] > numeros[1]){
+            System.out.println("\n " + numeros[0] + " é maior que " + numeros[1]);
+        } else {
+            System.out.println("\n " + numeros[1] + " é maior que " + numeros[0]);
+        }
+
+        scanner.close();
+    }
+}
