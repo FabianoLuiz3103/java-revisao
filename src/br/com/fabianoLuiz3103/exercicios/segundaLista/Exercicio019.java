053229e578c043999f82a8b737b99adc34d6f47a
FabianoLuiz3103 <fabianojesus1991@gmail.com>
2025-01-13 11:11:10 -0300

    Exercício 19 da segunda lista

---
+++ src/br/com/fabianoLuiz3103/exercicios/segundaLista/Exercicio019.java
@@ -0,0 +1,41 @@
+package br.com.fabianoLuiz3103.exercicios.segundaLista;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+/**
+ * @author FabianoLuiz3103
+ * --> Programa que recebe um número e informa se ele é inteiro ou decimal
+ */
+public class Exercicio019 {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        double numeroInformado, numeroArredondado;
+
+        do{
+            try{
+                System.out.println("\n\tInforme um número: ");
+                numeroInformado = scanner.nextDouble();
+                break;
+            }catch (InputMismatchException e){
+                System.out.println("\n\tERRO! O valor informado deve ser um número! ");
+                scanner.nextLine();
+            }
+        }while (true);
+
+        //é uma opção de resolução!
+//        numeroArredondado = Math.ceil(numeroInformado);
+//        if(numeroInformado == numeroArredondado){
+
+
+        if(numeroInformado == (int) numeroInformado){
+            System.out.println("\n\tO NÚMERO " + numeroInformado + " É UM NÚMERO INTEIRO! ");
+        }else {
+            System.out.println("\n\tO NÚMERO " + numeroInformado + " É UM NÚMERO DECIMAL! ");
+        }
+
+        scanner.close();
+    }
+}
