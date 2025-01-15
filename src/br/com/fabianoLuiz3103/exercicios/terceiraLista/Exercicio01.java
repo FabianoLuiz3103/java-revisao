8910ea3ba75efa76929747f6e78e03228e2851f0
FabianoLuiz3103 <fabianojesus1991@gmail.com>
2025-01-14 21:09:29 -0300

    Exercício 01 da terceira lista

---
+++ src/br/com/fabianoLuiz3103/exercicios/terceiraLista/Exercicio01.java
@@ -0,0 +1,35 @@
+package br.com.fabianoLuiz3103.exercicios.terceiraLista;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+/**
+ * @author FabianoLuiz3103
+ * --> Programa que pede uma nota entre 0 e 10 e informa se tiver inválida
+ * e continua pedindo até entrar um valor válido
+ */
+public class Exercicio01 {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        double nota;
+
+            while (true){
+
+                try{
+                    System.out.println("\n\tInforme uma nota entre 0 e 10: ");
+                    nota = scanner.nextDouble();
+                    if(nota >= 0  && nota <= 10){
+                        System.out.println("\n\tA nota informada foi: " + nota + " ela é válida! ");
+                        break;
+                    }
+                    System.out.println("\n\tERRO! A nota deve estar entre 0 e 10. ");
+                }catch (InputMismatchException e){
+                    System.out.println("\n\tERRO! A nota deve ser do tipo numérico! ");
+                    scanner.nextLine();
+                }
+        }
+            scanner.close();
+    }
+}
