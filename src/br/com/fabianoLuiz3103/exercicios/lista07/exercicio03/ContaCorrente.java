5bd02cef5410d3c8a1ecb1181e08fb8bc9089146
FabianoLuiz3103 <fabianojesus1991@gmail.com>
2025-01-27 09:54:42 -0300

    Exercicio 03 da sétima lista

---
+++ src/br/com/fabianoLuiz3103/exercicios/lista07/exercicio03/ContaCorrente.java
@@ -0,0 +1,9 @@
+package br.com.fabianoLuiz3103.exercicios.lista07.exercicio03;
+
+public class ContaCorrente {
+
+    String numeroConta;
+    double saldo;
+    boolean especial;
+    double limite;
+}
