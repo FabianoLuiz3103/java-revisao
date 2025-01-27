57ae91f3616ff2ccb64ee7a167ae4533415127d1
FabianoLuiz3103 <fabianojesus1991@gmail.com>
2025-01-27 10:02:26 -0300

    Revisão de métodos simples (sem parâmetros e sem retorno) em classes java

---
+++ src/br/com/fabianoLuiz3103/revisao/orientacaoObjetos/revMetodosSimples/Carro.java
@@ -0,0 +1,17 @@
+package br.com.fabianoLuiz3103.revisao.orientacaoObjetos.revMetodosSimples;
+
+/**
+ * revisão de métodos símples, sem parâmetros e sem retorno!
+ */
+public class Carro {
+
+    String marca;
+    String modelo;
+    int numPassageiros;
+    double capCombustivel;
+    double consumoCombustivel;
+
+    void exibirAutonomia(){
+        System.out.println("A autonomia do carro é: " + capCombustivel * consumoCombustivel + "km");
+    }
+}
