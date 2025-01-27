a7e58d6591c2621aa100f1abc7785b23f0c043a1
FabianoLuiz3103 <fabianojesus1991@gmail.com>
2025-01-27 09:28:10 -0300

    Criando a classe Carro - aula 01 revisão Orientação a Objetos

---
+++ src/br/com/fabianoLuiz3103/revisao/orientacaoObjetos/rv01/Carro.java
@@ -0,0 +1,12 @@
+package br.com.fabianoLuiz3103.revisao.orientacaoObjetos.rv01;
+
+public class Carro {
+
+    String marca;
+    String modelo;
+    int numPassageiros;
+    double capCombustivel;
+    double consumoCombustivel;
+}
+
+
