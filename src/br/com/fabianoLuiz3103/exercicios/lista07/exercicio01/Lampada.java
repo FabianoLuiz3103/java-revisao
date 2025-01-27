cf579719af2d6ea97294e117d7c804575aa69685
FabianoLuiz3103 <fabianojesus1991@gmail.com>
2025-01-27 09:44:59 -0300

    Exercicio 01 da sexta lista

---
+++ src/br/com/fabianoLuiz3103/exercicios/lista07/exercicio01/Lampada.java
@@ -0,0 +1,12 @@
+package br.com.fabianoLuiz3103.exercicios.lista07.exercicio01;
+
+/**
+ * @author FabianoLuiz
+ */
+public class Lampada {
+
+    String marca;
+    int tensao;
+    int potencia;
+    double preco;
+}
---
+++ src/br/com/fabianoLuiz3103/exercicios/lista07/exercicio01/TesteLampada.java
@@ -0,0 +1,18 @@
+package br.com.fabianoLuiz3103.exercicios.lista07.exercicio01;
+
+/**
+ * @author FabianoLuiz
+ */
+public class TesteLampada {
+
+    public static void main(String[] args) {
+
+        Lampada lampada01 = new Lampada();
+        lampada01.marca = "OuroLux";
+        lampada01.tensao = 220;
+        lampada01.potencia = 12;
+        lampada01.preco = 18.50;
+
+        System.out.println(lampada01.marca);
+    }
+}
