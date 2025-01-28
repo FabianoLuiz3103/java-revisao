9d6cc2525575408059567fdaa38e53c4b1b341b4
FabianoLuiz3103 <fabianojesus1991@gmail.com>
2025-01-28 13:19:20 -0300

    Revisão sobre encapsulamento, métodos get e set

--- src/br/com/fabianoLuiz3103/revisao/orientacaoObjetos/revModificadoresAcessoEncapsulamento/Carro.java
+++ src/br/com/fabianoLuiz3103/revisao/orientacaoObjetos/revModificadoresAcessoEncapsulamento/Carro.java
@@ -5,16 +5,58 @@ package br.com.fabianoLuiz3103.revisao.orientacaoObjetos.revModificadoresAcessoE
  * public --> todos podem ver
  * private --> somente a própia classe pode ver
  * default --> só pode ser visto dentro do próprio pacote
+ *
+ * encapsulamento -> métodos get e set
+ * os atributoa de uma classe devem ser private
  */
 class Carro {
 
-    String marca;
-    String modelo;
-    int numPassageiros;
-    double capCombustivel;
-    double consumoCombustivel;
+    private String marca;
+    private String modelo;
+    private int numPassageiros;
+    private double capCombustivel;
+    private double consumoCombustivel;
 
-    //--> Sem THIS
+    public String getMarca(){
+        return this.marca;
+    }
+
+    public void setMarca(String marca){
+        this.marca = marca;
+    }
+
+    public String getModelo(){
+        return this.modelo;
+    }
+
+    public void setModelo(String modelo){
+        this.modelo = modelo;
+    }
+
+    public int getNumPassageiros(){
+        return this.numPassageiros;
+    }
+
+    public void setNumPassageiros(int numPassageiros){
+        this.numPassageiros = numPassageiros;
+    }
+
+    public double getCapCombustivel(){
+        return this.capCombustivel;
+    }
+
+    public void setCapCombustivel(double capCombustivel){
+        this.capCombustivel = capCombustivel;
+    }
+
+    public double getConsumoCombustivel(){
+        return this.consumoCombustivel;
+    }
+
+    public void setConsumoCombustivel(double consumoCombustivel){
+        this.consumoCombustivel = consumoCombustivel;
+    }
+
     void exibirAutonomia(){
         System.out.println("A autonomia do carro é: " + capCombustivel * consumoCombustivel + "km");
     }
--- src/br/com/fabianoLuiz3103/revisao/orientacaoObjetos/revModificadoresAcessoEncapsulamento/TesteCarro.java
+++ src/br/com/fabianoLuiz3103/revisao/orientacaoObjetos/revModificadoresAcessoEncapsulamento/TesteCarro.java
@@ -5,11 +5,12 @@ public class TesteCarro {
     public static void main(String[] args) {
 
         Carro van = new Carro();
-        van.marca="Fiat";
-        van.modelo="Ducato";
-        van.numPassageiros=10;
-        van.capCombustivel=100;
-        van.consumoCombustivel=0.2;
+        van.setMarca("Fiat");
+        van.setModelo("Ducato");
+        van.setNumPassageiros(10);
+        van.setCapCombustivel(100);
+        van.setConsumoCombustivel(0.2);
 
+        System.out.println(van.getMarca() + " | " + van.getModelo());
     }
 }
