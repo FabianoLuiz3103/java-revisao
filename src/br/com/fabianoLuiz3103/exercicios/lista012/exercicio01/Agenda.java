3fb4729f9fc51407da0bece7c7450e3e441b7c02
FabianoLuiz3103 <fabianojesus1991@gmail.com>
2025-01-29 11:07:22 -0300

    Exercício 01 - lista 12 -> criando agenda e contato

---
+++ src/br/com/fabianoLuiz3103/exercicios/lista012/exercicio01/Agenda.java
@@ -0,0 +1,23 @@
+package br.com.fabianoLuiz3103.exercicios.lista012.exercicio01;
+
+public class Agenda {
+
+    private String nome;
+    private Contato[] contatos;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Contato[] getContatos() {
+        return contatos;
+    }
+
+    public void setContatos(Contato[] contatos) {
+        this.contatos = contatos;
+    }
+}
