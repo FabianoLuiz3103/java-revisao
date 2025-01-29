f9747d50a412d399af1a579a62be5489e9ff9151
FabianoLuiz3103 <fabianojesus1991@gmail.com>
2025-01-29 13:06:03 -0300

    Revisão Herança - classes Aluno e Professor

---
+++ src/br/com/fabianoLuiz3103/revisao/orientacaoObjetos/revHeranca/Aluno.java
@@ -0,0 +1,16 @@
+package br.com.fabianoLuiz3103.revisao.orientacaoObjetos.revHeranca;
+
+public class Aluno {
+
+    private String nome;
+    private String endereco;
+    private String telefone;
+    private String cpf;
+    private String[] cursos;
+    private String[][] notas;
+
+    public double calcularMedia(){return 0.0;}
+
+    public double verificarAprovado(){return 0.0;}
+
+}
---
+++ src/br/com/fabianoLuiz3103/revisao/orientacaoObjetos/revHeranca/Professor.java
@@ -0,0 +1,14 @@
+package br.com.fabianoLuiz3103.revisao.orientacaoObjetos.revHeranca;
+
+public class Professor {
+
+    private String nome;
+    private String endereco;
+    private String telefone;
+    private String cpf;
+    private String departamento;
+    private String nomeCurso;
+    private double salario;
+
+    public double calcularSalarioLiquido(){return 0.0;}
+}
