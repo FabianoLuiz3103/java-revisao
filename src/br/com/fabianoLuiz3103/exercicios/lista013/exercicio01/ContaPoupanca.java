f82d8229d2e90fa83a1490a6a714e095b8919094
FabianoLuiz3103 <fabianojesus1991@gmail.com>
2025-01-30 14:12:47 -0300

    Exercicio 01 lista 13 - refatorando conta poupança

--- src/br/com/fabianoLuiz3103/exercicios/lista013/exercicio01/ContaPoupanca.java
+++ src/br/com/fabianoLuiz3103/exercicios/lista013/exercicio01/ContaPoupanca.java
@@ -1,32 +1,38 @@
 package br.com.fabianoLuiz3103.exercicios.lista013.exercicio01;
 
+import java.util.Calendar;
+
 public class ContaPoupanca extends ContaBancaria{
 
-    private String diaRendimento;
+    private int diaRendimento;
 
-    public ContaPoupanca(String nomeCliente, String numConta, double saldo, String diaRendimento) {
+    public ContaPoupanca(String nomeCliente, String numConta, double saldo) {
         super(nomeCliente, numConta, saldo);
-        this.diaRendimento = diaRendimento;
     }
 
-    public String getDiaRendimento() {
+    public int getDiaRendimento() {
         return diaRendimento;
     }
 
-    public void setDiaRendimento(String diaRendimento) {
+    public void setDiaRendimento(int diaRendimento) {
         this.diaRendimento = diaRendimento;
     }
 
-    public void calcularNovoSaldo(double taxaRendimento){
-        if(taxaRendimento > 0){
+    public boolean calcularNovoSaldo(double taxaRendimento){
+        Calendar hoje = Calendar.getInstance();
+        if(diaRendimento == hoje.get(Calendar.DAY_OF_MONTH)){
             double novoSaldo = (super.getSaldo()*taxaRendimento)+super.getSaldo();
             super.setSaldo(novoSaldo);
+            return true;
+        }else{
+            return false;
         }
     }
 
     @Override
     public String toString() {
-        return "ContaPoupanca{" +
+        return "Cliente: " + super.toString() +
+                "ContaPoupanca{" +
                 "diaRendimento='" + diaRendimento + '\'' +
                 '}';
     }
