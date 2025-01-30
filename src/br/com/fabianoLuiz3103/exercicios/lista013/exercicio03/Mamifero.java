4613f99cad010d260edf8ffc31cf64354950cfe9
FabianoLuiz3103 <fabianojesus1991@gmail.com>
2025-01-30 17:38:04 -0300

    Exercicio 03 lista 13 - criando classe mamifero

---
+++ src/br/com/fabianoLuiz3103/exercicios/lista013/exercicio03/Mamifero.java
@@ -0,0 +1,26 @@
+package br.com.fabianoLuiz3103.exercicios.lista013.exercicio03;
+
+public class Mamifero extends Animal{
+
+    private String alimento;
+
+    public Mamifero(String nome, double comprimento, String cor, double velocidade, String alimento) {
+        super(nome, comprimento, cor, "terra", velocidade);
+        this.alimento = alimento;
+    }
+
+    public String getAlimento() {
+        return alimento;
+    }
+
+    public void setAlimento(String alimento) {
+        this.alimento = alimento;
+    }
+
+    @Override
+    public String toString() {
+        return "Mamifero{" +
+                "alimento='" + alimento + '\'' +
+                "}"+super.toString();
+    }
+}
