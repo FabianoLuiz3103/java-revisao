566df658e9c49c5e789e699e72ef122716a7be83
FabianoLuiz3103 <fabianojesus1991@gmail.com>
2025-01-30 17:00:30 -0300

    Exercicio 02 lista 13 - teste pessoa jurídica

--- src/br/com/fabianoLuiz3103/exercicios/lista013/exercicio02/Teste.java
+++ src/br/com/fabianoLuiz3103/exercicios/lista013/exercicio02/Teste.java
@@ -5,11 +5,12 @@ public class Teste {
     public static void main(String[] args) {
 
         Pessoa pessoaJuridica1 = new PessoaJuridica("Fabiano Luiz", "53824452276");
-        fazerCalculoIR(pessoaJuridica1, 35000, pessoaJuridica1.getNome());
         Pessoa pessoaJuridica2 = new PessoaJuridica("Yolanda B.", "53824452111");
-        fazerCalculoIR(pessoaJuridica2, 1000, pessoaJuridica2.getNome());
         Pessoa pessoaJuridica3 = new PessoaJuridica("Carlos E.", "11824452288");
+        fazerCalculoIR(pessoaJuridica1, 35000, pessoaJuridica1.getNome());
+        fazerCalculoIR(pessoaJuridica2, 1000, pessoaJuridica2.getNome());
         fazerCalculoIR(pessoaJuridica3, 15000, pessoaJuridica3.getNome());
+
     }
 
     private static void fazerCalculoIR(Pessoa pessoa, double rendaBruta, String nome){
