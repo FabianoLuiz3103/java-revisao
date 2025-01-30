f4586304a2ca89a902faa999f0e5fc603354795e
FabianoLuiz3103 <fabianojesus1991@gmail.com>
2025-01-30 16:17:50 -0300

    Exercicio 02 lista 13 - criando a classe pessoa

---
+++ src/br/com/fabianoLuiz3103/exercicios/lista013/exercicio02/Pessoa.java
@@ -0,0 +1,30 @@
+package br.com.fabianoLuiz3103.exercicios.lista013.exercicio02;
+
+public abstract class Pessoa {
+
+    private String nome;
+    private String cpf;
+
+    public Pessoa(String nome, String cpf) {
+        this.nome = nome;
+        this.cpf = cpf;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public abstract double calcularImposto(double rendaBruta);
+}
