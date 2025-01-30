bbc22fd7d98e0abc03e44d5fcabd0a7fb5128b8a
FabianoLuiz3103 <fabianojesus1991@gmail.com>
2025-01-30 17:20:30 -0300

    Exercicio 02 lista 13 - refatorando classe pessoa jurídica

--- src/br/com/fabianoLuiz3103/exercicios/lista013/exercicio02/PessoaJuridica.java
+++ src/br/com/fabianoLuiz3103/exercicios/lista013/exercicio02/PessoaJuridica.java
@@ -2,17 +2,31 @@ package br.com.fabianoLuiz3103.exercicios.lista013.exercicio02;
 
 public class PessoaJuridica extends Pessoa{
 
+    private String cnpj;
 
-    public PessoaJuridica(String nome, String cpf) {
-        super(nome, cpf);
+    public PessoaJuridica(String nome, double rendaBruta, String cnpj) {
+
+        super(nome, rendaBruta);
+        this.cnpj = cnpj;
+    }
+
+    public String getCnpj() {
+        return cnpj;
+    }
+
+    public void setCnpj(String cnpj) {
+        this.cnpj = cnpj;
     }
 
     @Override
-    public double calcularImposto(double rendaBruta) {
-        if(rendaBruta>0){
-            return rendaBruta*0.10;
-        }
-        return 0;
+    public double calcularImposto() {
+        return (super.getRendaBruta()*0.1);
     }
 
+    @Override
+    public String toString() {
+        return "PessoaJuridica{" +
+                "cnpj='" + cnpj + '\'' +
+                '}' + super.toString();
+    }
 }
