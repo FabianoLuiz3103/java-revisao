723e3ccad1b1db3a3a70ce13b75c603017646815
FabianoLuiz3103 <fabianojesus1991@gmail.com>
2025-01-31 11:33:16 -0300

    Revisão interfaces - classe ave e mamifero herdam de animal

--- src/br/com/fabianoLuiz3103/revisao/orientacaoObjetos/revInterfaces/Ave.java
+++ src/br/com/fabianoLuiz3103/revisao/orientacaoObjetos/revInterfaces/Ave.java
@@ -1,6 +1,8 @@
 package br.com.fabianoLuiz3103.revisao.orientacaoObjetos.revInterfaces;
 
-public abstract class Ave {
+import br.com.fabianoLuiz3103.exercicios.lista013.exercicio03.Animal;
+
+public abstract class Ave extends Animal {
 
     public abstract String voar();
 }
--- src/br/com/fabianoLuiz3103/revisao/orientacaoObjetos/revInterfaces/Mamifero.java
+++ src/br/com/fabianoLuiz3103/revisao/orientacaoObjetos/revInterfaces/Mamifero.java
@@ -1,6 +1,8 @@
 package br.com.fabianoLuiz3103.revisao.orientacaoObjetos.revInterfaces;
 
-public abstract class Mamifero {
+import br.com.fabianoLuiz3103.exercicios.lista013.exercicio03.Animal;
+
+public abstract class Mamifero extends Animal {
 
     public abstract String amamentar();
 }
