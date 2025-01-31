c24f0e21572c141c0cda087b0932150866b591ca
FabianoLuiz3103 <fabianojesus1991@gmail.com>
2025-01-31 11:41:44 -0300

    Revisão interfaces - classe cachorro

---
+++ src/br/com/fabianoLuiz3103/revisao/orientacaoObjetos/revInterfaces/Cachorro.java
@@ -0,0 +1,33 @@
+package br.com.fabianoLuiz3103.revisao.orientacaoObjetos.revInterfaces;
+
+public class Cachorro extends Mamifero{
+
+    private String tamanho;
+    private String raca;
+
+    public String getTamanho() {
+        return tamanho;
+    }
+
+    public void setTamanho(String tamanho) {
+        this.tamanho = tamanho;
+    }
+
+    public String getRaca() {
+        return raca;
+    }
+
+    public void setRaca(String raca) {
+        this.raca = raca;
+    }
+
+    @Override
+    public String emitirSom() {
+        return null;
+    }
+
+    @Override
+    public String amamentar() {
+        return null;
+    }
+}
