e17663d1d51c07b1b177e10bb912a58a56b2b367
FabianoLuiz3103 <fabianojesus1991@gmail.com>
2025-01-31 14:30:58 -0300

    Exercicio lista 14 - Classes abstratas -> Figura geométrica, Figura 2d e Figura 3d

---
+++ src/br/com/fabianoLuiz3103/exercicios/lista014/Figura2D.java
@@ -0,0 +1,8 @@
+package br.com.fabianoLuiz3103.exercicios.lista014;
+
+public abstract class Figura2D extends FiguraGeometrica implements DimensaoSuperficial{
+
+    public Figura2D(String nome, String cor) {
+        super(nome, cor);
+    }
+}
---
+++ src/br/com/fabianoLuiz3103/exercicios/lista014/Figura3D.java
@@ -0,0 +1,8 @@
+package br.com.fabianoLuiz3103.exercicios.lista014;
+
+public abstract class Figura3D extends FiguraGeometrica implements DimensaoSuperficial, DimensaoVolumetrica{
+
+    public Figura3D(String nome, String cor) {
+        super(nome, cor);
+    }
+}
