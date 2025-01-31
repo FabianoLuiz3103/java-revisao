2a45c03570d6aa62bc56ac7d78cbe7c18fa2623d
FabianoLuiz3103 <fabianojesus1991@gmail.com>
2025-01-31 11:40:17 -0300

    Revisão interfaces - classe papagaio

---
+++ src/br/com/fabianoLuiz3103/revisao/orientacaoObjetos/revInterfaces/Papagaio.java
@@ -0,0 +1,15 @@
+package br.com.fabianoLuiz3103.revisao.orientacaoObjetos.revInterfaces;
+
+public class Papagaio extends Ave {
+
+
+    @Override
+    public String voar() {
+        return null;
+    }
+
+    @Override
+    public String emitirSom() {
+        return null;
+    }
+}
