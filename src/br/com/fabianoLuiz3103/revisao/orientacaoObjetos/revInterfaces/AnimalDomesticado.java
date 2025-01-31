574ff912d2b72d82f201542009c351ec4eda902e
FabianoLuiz3103 <fabianojesus1991@gmail.com>
2025-01-31 12:04:21 -0300

    Revisão interfaces -

--- src/br/com/fabianoLuiz3103/revisao/orientacaoObjetos/revInterfaces/AnimalDomesticado.java
+++ src/br/com/fabianoLuiz3103/revisao/orientacaoObjetos/revInterfaces/AnimalDomesticado.java
@@ -1,5 +1,10 @@
 package br.com.fabianoLuiz3103.revisao.orientacaoObjetos.revInterfaces;
 
+/**
+ * Todos os atributos dentro de uma interface são do
+ * tipo public final -> Muito comum achar interface de
+ * constantes
+ */
 public interface AnimalDomesticado {
 
     void levarVeterinario();
