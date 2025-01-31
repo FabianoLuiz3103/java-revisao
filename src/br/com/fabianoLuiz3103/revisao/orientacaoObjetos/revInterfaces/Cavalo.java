9f2626a758b493edf72ecb360a480e113ba43b0c
FabianoLuiz3103 <fabianojesus1991@gmail.com>
2025-01-31 12:01:20 -0300

    Revisão interfaces - classe cavalo

---
+++ src/br/com/fabianoLuiz3103/revisao/orientacaoObjetos/revInterfaces/Cavalo.java
@@ -0,0 +1,24 @@
+package br.com.fabianoLuiz3103.revisao.orientacaoObjetos.revInterfaces;
+
+public class Cavalo extends Mamifero implements AnimalDomesticado{
+
+    @Override
+    public void emitirSom() {
+
+    }
+
+    @Override
+    public void amamentar() {
+
+    }
+
+    @Override
+    public void levarVeterinario() {
+
+    }
+
+    @Override
+    public void alimentar() {
+
+    }
+}
