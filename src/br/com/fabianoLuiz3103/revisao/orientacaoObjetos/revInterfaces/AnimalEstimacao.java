0987cfdbf4d5f02388bce44e87c0e518b36f78fa
FabianoLuiz3103 <fabianojesus1991@gmail.com>
2025-01-31 11:58:56 -0300

    Revisão interfaces - interface Animal estimação

---
+++ src/br/com/fabianoLuiz3103/revisao/orientacaoObjetos/revInterfaces/AnimalEstimacao.java
@@ -0,0 +1,11 @@
+package br.com.fabianoLuiz3103.revisao.orientacaoObjetos.revInterfaces;
+
+/**
+ * Todos os métodos de uma interface são, de forma implícita
+ * públicos e abstratos
+ */
+public interface AnimalEstimacao {
+
+    void brincar();
+    void levarPassear();
+}
